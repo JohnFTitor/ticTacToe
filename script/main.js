00331abd097a118ecd850272af1bdc05575a47a9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-09-15 17:57:26 -0500

    Moved the logic to the Game module

--- script/main.js
+++ script/main.js
@@ -6,9 +6,7 @@ const Game = (function() {
                          ["", "", ""]];
     
         const add = (movement,row,column) => {
-            if (row <= 2 && column <=2) {
-                gameboard[row][column] = movement;
-            }
+            gameboard[row][column] = movement;
         }
     
         const displayArray = () => {
@@ -27,8 +25,8 @@ const Game = (function() {
         let playerMovement = movement;
     
         const play = () => {
-            const row = prompt('which row are you gonna choose? ', '0');
-            const column = prompt('which column r u gonna choose?', '0');
+            const row = prompt('which row are you gonna choose? ' + playerName, '0');
+            const column = prompt('which column r u gonna choose?' + playerName, '0');
             GameBoard.add(playerMovement, row, column);
             console.log(`${playerName} has made its move`);
             GameBoard.display();
@@ -40,11 +38,22 @@ const Game = (function() {
     }
 
     const start = () => {
-        let player1 = prompt('State your name, Player');
-        let player1Movement = prompt('Which symbol are you gonna use?');
+        const p1 = prompt('State your name, Player');
+        const p1Movement = prompt('Which symbol are you gonna use?');
+        const p2 = prompt('State your name, Player 2');
+        const p2Movement = prompt('Same question. Which symbol are you gonna use?');
+        
+        const player1 = Player(p1,p1Movement);
+        const player2 = Player(p2,p2Movement);
+
+        for (let i = 0; i < 4; i++) {
+            player1.play();
+            player2.play();
+        }
     }
     
     return {
         start: start
     }
+    
 })()
